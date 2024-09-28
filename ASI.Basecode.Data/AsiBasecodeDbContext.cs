bdc326a0650ddfb704e706a2d410a3c54d3085da
Soliken <kennethrex456@gmail.com>
2024-09-28 14:00:28 +0800

    Merge

--- ASI.Basecode.Data/AsiBasecodeDbContext.cs
+++ ASI.Basecode.Data/AsiBasecodeDbContext.cs
@@ -24,10 +24,6 @@ namespace ASI.Basecode.Data
         {
             if (!optionsBuilder.IsConfigured)
             {
-<<<<<<< HEAD
-=======
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
->>>>>>> 30b5a31b290f6245521af693791e713d6233c53c
                 optionsBuilder.UseSqlServer("Addr=(LocalDB)\\MSSQLLocalDB;database=AsiBasecodeDb;Integrated Security=False;Trusted_Connection=True");
             }
         }
