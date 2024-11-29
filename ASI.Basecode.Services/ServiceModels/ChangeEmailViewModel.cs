a34596e407a472d4f32ef79cdb774e0c93d8162c
Yancy Louis S. Luceño <yancy2boy@gmail.com>
2024-11-30 03:08:55 +0800

    Newly added viewmodels (for updating username and email)

---
+++ ASI.Basecode.Services/ServiceModels/ChangeEmailViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ASI.Basecode.Services.ServiceModels
+{
+    public class ChangeEmailViewModel
+    {
+        [Required(ErrorMessage = "Email is required.")]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress(ErrorMessage = "Invalid email format.")]
+        public string NewEmail { get; set; }
+    }
+}
