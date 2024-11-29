61b57d604aa612de7248feff112395acd3a7c1bd
Yancy Louis S. Luceño <yancy2boy@gmail.com>
2024-11-30 03:11:34 +0800

    Property Deleted (ChangeEmailOrUsernameViewModel)

--- ASI.Basecode.Services/ServiceModels/ProfileViewModel.cs
+++ ASI.Basecode.Services/ServiceModels/ProfileViewModel.cs
@@ -3,8 +3,9 @@
     public class ProfileViewModel
     {
         public ChangePasswordViewModel UpdatePassword { get; set; }
+        public ChangeEmailViewModel UpdateEmail { get; set; }
+        public ChangeUsernameViewModel UpdateUsername { get; set; }
         public bool IsPasswordChangeRequired { get; set; } = false;
-        public ChangeEmailOrUsernameViewModel UpdateEmailOrUserName { get; set; }
         public string UserCode { get; set; }
         public string Email { get; set; }
         public string ProfilePicture { get; set; }
